601f76b43ddccf130c8f4396dc94216ebf8b11b3
nsw2022 <117246890+nsw2022@users.noreply.github.com>
2024-11-28 16:47:20 +0900

    [level 0] Title: 암호 해독, Time: 0.96 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,9 @@
+def solution(cipher, code):
+    answer = ''
+    
+    for i in range(code, len(cipher)+1, code):
+        answer += cipher[i - 1]
+        #print(f'code: {}, cipher[code] {}', code, answer)
+        print(f'len(cipher): {len(cipher)}, code: {i}, cipher[{i - 1}] {cipher[i - 1]}') 
+        
+    return answer
